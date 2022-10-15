29005f1b99770823f802b5ab4c9ba7f93803af08
Alba Mg <alba.martinez.mt@usa.edu.co>
2022-10-15 14:43:14 -0500

    Actualización5

---
+++ src/main/java/com/ciclo3/projectc3/Repository/CrudRepository/ScoreCrudRepository.java
@@ -0,0 +1,9 @@
+package com.ciclo3.projectc3.Repository.CrudRepository;
+
+
+import com.ciclo3.projectc3.Entities.Score;
+import org.springframework.data.repository.CrudRepository;
+
+
+public interface ScoreCrudRepository extends CrudRepository<Score, Integer> {
+}
---
+++ src/main/java/com/ciclo3/projectc3/Service/ScoreService.java
@@ -0,0 +1,36 @@
+package com.ciclo3.projectc3.Service;
+
+import com.ciclo3.projectc3.Entities.Reservation;
+import com.ciclo3.projectc3.Entities.Score;
+import com.ciclo3.projectc3.Repository.ReservationRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ScoreService {
+
+    private ScoreService scoreService;
+
+    public List<Score> getAll(){
+        return scoreService.getAll();
+    }
+
+    public Optional<Score> getScore(int id){
+        return scoreService.getScore(id);
+    }
+
+    public Score save (Score score){
+        if (score.getIdScore() == null){
+            return scoreService.save(score);
+        } else {
+            Optional<Score> score1 = scoreService.getScore(score.getIdScore());
+            if(score1.isPresent()){
+                return scoreService.save(score);
+            } else {
+                return score;
+            }
+        }
+    }
+}
